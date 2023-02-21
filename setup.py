4da611d9e856cf07bb49a0ee42fb9e67b0364fd1
EKOzkan <44098870+EKOzkan@users.noreply.github.com>
2023-02-21 06:15:57 +0300

    Added test id mode, chromedriver mode, manual cookie creation option, unretweet function, and some bug fixes.

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='twAuto',
-    version='0.3.1',
+    version='0.3.4',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='TwAuto is a library for testing your code on pre-Twitter API application stage.',
-    long_description=open('README.md').read(),
+    long_description=open('README.md',  encoding="utf8").read(),
     long_description_content_type='text/markdown',
-    install_requires=['selenium', 'bs4'],
+    install_requires=['selenium', 'bs4', 'webdriver-manager'],
     url='https://github.com/EKOzkan/twAuto',
     author='Ekin Kagan Ozkan',
     author_email='ekinkagan@gmail.com'
