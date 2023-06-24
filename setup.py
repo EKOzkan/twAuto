d83026e78dc0782a6f4d818b765ac83be4698334
EKOzkan <44098870+EKOzkan@users.noreply.github.com>
2023-06-24 03:06:03 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description='TwAuto is a library for testing your code on pre-Twitter API application stage.',
     long_description=open('README.md',  encoding="utf8").read(),
     long_description_content_type='text/markdown',
-    install_requires=['selenium', 'bs4', 'webdriver-manager'],
+    install_requires=['selenium==4.4.3', 'bs4', 'webdriver-manager'],
     url='https://github.com/EKOzkan/twAuto',
     author='Ekin Kagan Ozkan',
     author_email='ekinkagan@gmail.com'
